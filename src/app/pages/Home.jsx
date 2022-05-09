947ad8aa599340bce44fc167da68c78648f7b38b
Nicolas Sierra <nico.sierra13@gmail.com>
2022-05-08 21:19:18 -0500

    fix css clash with other branch

--- src/app/pages/Home.jsx
+++ src/app/pages/Home.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
 const Home = () => {
   return (
     <div className="home">
       <Navbar />
+      <div className="hero">
+        <Hero />
+      </div>
     </div>
   );
 };
