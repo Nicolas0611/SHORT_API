398d5bf4fa0cf257e5f5e016fa0b0edb37d2f412
Nicolas Sierra <nico.sierra13@gmail.com>
2022-05-21 14:41:53 -0500

    Advance statistics cards

---
+++ src/app/libraries/data.jsx
@@ -0,0 +1,24 @@
+import brandRecognition from "../../assets/images/icon-brand-recognition.svg";
+import detailedRecords from "../../assets/images/icon-detailed-records.svg";
+import fullyCustom from "../../assets/images/icon-fully-customizable.svg";
+
+export const cardContent = [
+  {
+    title: "Brand Recognition",
+    parragraph:
+      "Boost your brand recognition with each click. Generic links don’t mean a thing. Branded links help instil confidence in your content.",
+    img: brandRecognition,
+  },
+  {
+    title: "Detailed Records",
+    parragraph:
+      "Gain insights into who is clicking your links. Knowing when and where people engage with your content helps inform better decisions.",
+    img: detailedRecords,
+  },
+  {
+    title: "Fully Customizable",
+    parragraph:
+      "Improve brand awareness and content discoverability through customizable links, supercharging audience engagement.",
+    img: fullyCustom,
+  },
+];
--- src/app/pages/Home.jsx
+++ src/app/pages/Home.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import Searchbar from "../components/Searchbar";
+import Cards from "../components/Cards";
+import { cardContent } from "../libraries/data";
+
 const Home = () => {
   return (
     <>
@@ -13,6 +16,24 @@ const Home = () => {
         <section className="search">
           <Searchbar />
         </section>
+        <section className="statistics container">
+          <h2 className="statistics-title"> Advanced Statistics</h2>
+          <p className="statistics-parragraph">
+            Track how your links are performing across the web with our advanced
+            statistics dashboard.
+          </p>
+          <div className="statistics-containerCards">
+            {cardContent.map((card, index) => (
+              <Cards
+                key={index}
+                title={card.title}
+                parragraph={card.parragraph}
+                img={card.img}
+              />
+            ))}
+          </div>
+        </section>
+        <section className="getStarted"></section>
       </main>
     </>
   );
